58175c8338c6b27e634679a85c993a152bce5f91
revqueenge <Dealul02@gmail.com>
2024-09-30 01:46:35 +0700

    Определяем чекбоксы внутри MainActivity и создаем функцию, что пока активен 1 чекбокс, другие выбрать нельзя, если их выбрать то флажок с предыдущего снимается

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.lab5
 
 import android.os.Bundle
+import android.widget.CheckBox
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var dollar: CheckBox
+    private lateinit var euro: CheckBox
+    private lateinit var funt: CheckBox
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,22 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        dollar = findViewById(R.id.dollar)
+        euro = findViewById(R.id.euro)
+        funt = findViewById(R.id.funt)
+        setCheckBoxListener(dollar)
+        setCheckBoxListener(euro)
+        setCheckBoxListener(funt)
+    }
+    private fun setCheckBoxListener(checkBox: CheckBox) {
+        checkBox.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // Сбрасываем другие чекбоксы
+                if (checkBox != dollar) dollar.isChecked = false
+                if (checkBox != euro) euro.isChecked = false
+                if (checkBox != funt) funt.isChecked = false
+            }
         }
     }
 }
